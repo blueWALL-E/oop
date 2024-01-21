d313896eaacf32ecdc207dd6cdc32434e40c34b5
blueWALL-E <boyu.dragon@gmail.com>
2024-01-21 15:24:25 +0800

    添加了speak方法

--- oop.c
+++ oop.c
@@ -1,7 +1,7 @@
 /*
  * @Author: Dragon
  * @Date: 2023-08-11 16:52:52
- * @LastEditTime: 2023-08-16 20:58:45
+ * @LastEditTime: 2024-01-21 15:23:10
  * @FilePath: \oop\oop.c
  * @Description:C语言面向对象尝试
  * @Wearing:  Read only, do not modify place!!!
@@ -22,10 +22,13 @@ typedef struct Animal         // 定义类的属性和方法
     // 方法
     void (*eat)(Animal *P_animal);
     void (*run)(Animal *P_animal, int speed);
+    void (*speak)(Animal *P_animal, char content[]);
 } Animal;
+
 // 类方法的声明
 void eat(Animal *P_animal);
 void run(Animal *P_animal, int speed);
+void speak(Animal *P_animal, char content[]);
 
 Animal *creatAnimal(int age, const char name[]);
 
@@ -37,6 +40,7 @@ int main()
     printf("name = %s\n", p_dog->name);
     p_dog->eat(p_dog);
     p_dog->run(p_dog, 100);
+    p_dog->speak(p_dog, "hello");
 
     return 0;
 }
@@ -51,14 +55,21 @@ void run(Animal *P_animal, int speed)
     printf("name:%s ", P_animal->name);
     printf("speed:%d\n", speed);
 }
+void speak(Animal *P_animal, char content[])
+{
+    char *buff = NULL;
+    buff = content;
+    printf("name:%s ", P_animal->name);
+    printf("content:%s\n", buff);
+}
 
 // 构造函数定义
 
 /**
- * @description:
- * @param {int} age
- * @param {char} name
- * @return {*}
+ * @description: 实例化一个Animal对象 带参数
+ * @param {int} age 年龄
+ * @param {char} name 名字
+ * @return {Animal*} 返回实例化类的地址
  */
 Animal *creatAnimal(int age, const char name[])
 {
@@ -73,6 +84,7 @@ Animal *creatAnimal(int age, const char name[])
     // 方法确定
     P_animal->eat = eat;
     P_animal->run = run;
+    P_animal->speak = speak;
     age++;
     return P_animal;
 }
